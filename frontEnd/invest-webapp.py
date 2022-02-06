c725b2ffbfb08e2553e7750e182dc874efb36c85
Andrew Kiggins <andrewkiggins2000@gmail.com>
2022-02-05 23:16:52 -0800

    front end input

--- frontEnd/invest-webapp.py
+++ frontEnd/invest-webapp.py
@@ -1,2 +1,20 @@
-import streamlit as s
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import datetime
+
+# App title
+st.markdown('''
+# Student investent app 
+Helping students make investment decisions with whatever money they have.
+''')
+st.write('---')
+
+# Sidebar
+st.sidebar.subheader('Investment parameters')
+investment_amount = st.sidebar.number_input("Investment amount",1)
+
+# Retrieving tickers data
+market_list = ["TSX","SP500","NASDAQ"]
+tickerSymbol = st.sidebar.selectbox('Market', market_list) # Select ticker symbol
 
