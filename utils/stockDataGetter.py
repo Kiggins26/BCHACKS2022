1ae3b9c1e5eec880ea9ac7a8badae14f258d31ee
Andrew Kiggins <andrewkiggins2000@gmail.com>
2022-02-05 19:25:44 -0800

    more optimized flow

--- utils/stockDataGetter.py
+++ utils/stockDataGetter.py
@@ -2,45 +2,48 @@ import yfinance as yf
 import streamlit as st
 
 @st.cache
-def tsxMarketInfo(investmentAmount):
+def tsxMarketInfo():
     tickersInfo = []
     with open("./tsx.txt") as tsxFile:
         for line in tsxFile:
             ticker = line.split()[0]
             print(ticker)
             stock = yf.Ticker(ticker)
-            if (stock.info['regularMarketPrice'] <= investmentAmount):
-                tickersInfo.append(stock.info)
+            tickersInfo.append(stock.info)
 
     return tickersInfo 
 
 
 @st.cache
-def sapMarketInfo(investmentAmount):
+def sapMarketInfo():
     tickersInfo = []
     with open("./sp500.txt") as tsxFile:
         for line in tsxFile:
             ticker = line.split()[0]
             print(ticker)
             stock = yf.Ticker(ticker)
-            if (stock.info['regularMarketPrice'] <= investmentAmount):
-                tickersInfo.append(stock.info)
+            tickersInfo.append(stock.info)
 
     return tickersInfo 
 
 
 @st.cache
-def nasdaqMarketInfo(investmentAmount):
+def nasdaqMarketInfo():
     tickersInfo = []
     with open("./nasdaq100.txt") as tsxFile:
         for line in tsxFile:
             ticker = line.split()[0]
             print(ticker)
             stock = yf.Ticker(ticker)
-            if (stock.info['regularMarketPrice'] <= investmentAmount):
-                tickersInfo.append(stock.info)
+            tickersInfo.append(stock.info)
 
     return tickersInfo 
 
+@st.cache
+def getValidListOfStocks(stockList, investmentAmount)
+    validList = []
+    for stock in stockList:
+        if (stock.info['regularMarketPrice'] <= investmentAmount):
+            validList.append(stock)
+    return validList
 
-nasdaqMarketInfo(10000000)
